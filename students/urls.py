a078251e0372165585ba8c8b41f2e713427cb3b4
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-06-22 14:21:51 +0100

    student/urls.py created and updated

---
+++ students/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.student_list, name='student_list'),
+    path('student/<int:pk>/', views.student_info, name='student_info'),
+    path('student/new/', views.student.create, name='student_create'),
+    path('student/<int:pk>/edit/', views.student_update, name='student_update'),
+    path('student/<int:pk>/delete/', views.student_delete, name='student_delete'),
+]
