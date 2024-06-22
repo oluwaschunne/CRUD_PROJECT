b4529022c23b74fecd8ff2163463ebc9569fb634
oluwaschunne <falopeoluwaseun2@gmail.com>
2024-06-22 21:42:17 +0100

    all corrections made

--- students/views.py
+++ students/views.py
@@ -5,12 +5,13 @@ from .forms import StudentForm
 # function to view all students
 def student_list(request):
     students = Student.objects.all()
-    return render(request, 'students/student_list.html', {'student': students})
+    print(students)
+    return render(request, 'students/student_list.html', {'students': students})
 
 # function to get student info
 def student_info(request, pk): # pk (primary key; a unique identifier for students)
     student = get_object_or_404(Student, pk=pk)
-    return render(request, 'students/student_info.html', {'stuent': student})
+    return render(request, 'students/student_info.html', {'student': student})
 
 # function to create new student
 def student_create(request):
@@ -32,11 +33,12 @@ def student_update(request, pk):
             form.save()
             return redirect('student_list')
     else:
-        form = StudentForm(isinstance=student)
+        form = StudentForm(instance=student)
     return render(request, 'students/student_form.html', {'form': form})
 
+# function to delete student info
 def student_delete(request, pk):
-    student = get_object_or_404(student, pk=pk)
+    student = get_object_or_404(Student, pk=pk)
     if request.method == "POST":
         student.delete()
         return redirect('student_list')
